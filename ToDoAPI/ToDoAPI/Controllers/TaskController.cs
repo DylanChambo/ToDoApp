794c8f92cbd649bafee946b17f1c1a67a29f12c0
DylanChambo <dylan.chambo@gmail.com>
2022-08-17 18:39:13 +1200

    feat: :sparkles: added post, put and delete requests

--- ToDoAPI/ToDoAPI/Controllers/TaskController.cs
+++ ToDoAPI/ToDoAPI/Controllers/TaskController.cs
@@ -16,10 +16,51 @@ namespace ToDoAPI.Controllers
             _context = context;
         }
 
+        // Get Request to get tasks
         [HttpGet]
-
         public async Task<ActionResult<List<Task>>> GetTasks()
         {
+            return Ok(await _context.Tasks.ToListAsync());
+        }
+
+        // Post Request to create a task
+        [HttpPost]
+        public async Task<ActionResult<List<Task>>> CreateTask(Task task)
+        {
+            _context.Tasks.Add(task);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Tasks.ToListAsync());
+        }
+
+        // Put Request to update a task
+        [HttpPut]
+        public async Task<ActionResult<List<Task>>> UpdateTask(Task task)
+        {
+            var dbTask = await _context.Tasks.FindAsync(task.Id);
+            if (dbTask == null)
+                return BadRequest("Task not found.");
+
+            dbTask.Info = task.Info;
+            dbTask.DueDate = task.DueDate;
+            dbTask.Status = task.Status;
+            dbTask.Priority = task.Priority;
+
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Tasks.ToListAsync());
+        }
+
+        // Delete Request to delete a task
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Task>>> DeleteTask(int id)
+        {
+            var dbTask = await _context.Tasks.FindAsync(id);
+            if (dbTask == null)
+                return BadRequest("Hero not found.");
+
+            _context.Tasks.Remove(dbTask);
+
+            await _context.SaveChangesAsync();
             return Ok(await _context.Tasks.ToListAsync());
         }
     }
